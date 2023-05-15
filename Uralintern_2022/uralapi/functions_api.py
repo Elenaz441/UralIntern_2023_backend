4821bd9a7b4a743c13855489b909d1f538e8c68f
EZ <Elena.Zasypkina@urfu.me>
2023-05-15 20:33:44 +0500

    Fix id teams

--- Uralintern_2022/uralapi/functions_api.py
+++ Uralintern_2022/uralapi/functions_api.py
@@ -7,7 +7,7 @@ def get_title_id(object, name_title):
         if name_title == 'intern':
             t = i.id_team.id
             a = Team.objects.get(pk=t)
-            result.append({'id': i.id, 'title': a.title})
+            result.append({'id': t, 'title': a.title})
         else:
             result.append({'id': i.id, 'title': i.title})
     return result
