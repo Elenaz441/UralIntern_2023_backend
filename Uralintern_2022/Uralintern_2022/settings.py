63521d3c5eebacc8deb7d2bfb6bb3874f1765ae8
sxkxixx <sasha.kornilov.1212@gmail.com>
2023-05-22 17:48:39 +0500

    Dockerfile in Uralintern_2022/ && docker-compose.yml

--- Uralintern_2022/Uralintern_2022/settings.py
+++ Uralintern_2022/Uralintern_2022/settings.py
@@ -18,19 +18,13 @@ from collections import OrderedDict
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-envpath = os.path.join(os.path.abspath(os.path.dirname(__file__)), '.env')
-if os.path.exists(envpath):
-    load_dotenv(envpath, override=True)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2pe!rbo3ybj%@k=8dbfzk&-c#)hfjm-4_ztyv6-w3ngg%oavj8'
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', True)
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '0.0.0.0').split()
 
 
 # Application definition
@@ -85,22 +79,15 @@ WSGI_APPLICATION = 'Uralintern_2022.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", ""),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", ""),
+        "HOST": os.environ.get("SQL_HOST", ""),
+        "PORT": os.environ.get("SQL_PORT", ""),
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': '',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
 
 
 # Password validation
